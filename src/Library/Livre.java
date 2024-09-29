b8cebd508f08da1101297748406a00676219aee8
walid885 <bettaieb88walid@gmail.com>
2024-09-29 15:46:13 +0100

    creating the two classes emprunt et Livre pour java , and setting up the livre.java attributes

---
+++ src/Library/Livre.java
@@ -0,0 +1,33 @@
+package Library;
+
+public class Livre {
+
+	private String codeLivre; 
+	private String Titre; 
+	
+	public Livre (String codeLivre, String Titre) {
+		this.codeLivre = codeLivre; 
+		this.Titre = Titre;
+		
+	}
+	public String getCodeLivre() {
+		return codeLivre;
+	}
+	public void setCodeLivre(String codeLivre) {
+		this.codeLivre = codeLivre;
+	}
+	public String  getTitre () {
+		return Titre;
+	}
+	
+	public void setTitre( String titre) {
+		this.Titre = Titre; 
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
